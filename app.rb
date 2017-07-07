0e1aa54993a6fcdf3a6130c71f22697ca8f766cc
iyke <nderiony95@gmail.com>
2017-07-07 16:38:07 +0300

    added new method on app.rb

--- app.rb
+++ app.rb
@@ -2,26 +2,51 @@ require('sinatra')
   require('sinatra/reloader')
   also_reload('lib/**/*.rb')
   require('./lib/member')
+  require('./lib/team')
 
   get('/') do
-    erb(:index)
-  end
+   erb(:index)
+ end
 
-  get('/members') do
-    @member = Member.all()
-    erb(:members)
-  end
+ get('/teams/new') do
+    erb(:teams_form)
+ end
 
-  post('/members') do
-    name = params.fetch("name")
-    age = params.fetch("age")
-    credentials = params.fetch("credentials")
-    @member = Member.new(name, age, credentials)
-    @member.save()
-    erb(:success)
-  end
+ get('/teams') do
+   @teams = Team.all()
+   erb(:teams)
+ end
 
-  get('/members/:id') do
-    @member = Member.find(params.fetch("id"))
-    erb(:member)
-  end
+ post('/teams') do
+   name = params.fetch('name')
+   Team.new(name).save()
+   @teams = Team.all()
+   erb(:success)
+ end
+
+ get('/members/:id') do
+   @member = Member.find(params.fetch('id').to_i())
+   erb(:member)
+ end
+
+ get('/teams/:id') do
+   @team = Team.find(params.fetch('id').to_i())
+   erb(:team)
+ end
+
+ get('/teams/:id/members/new') do
+     @team = Team.find(params.fetch('id').to_i())
+     erb(:team_members_form)
+ end
+
+ post('/members') do
+   name = params.fetch('name')
+   age = params.fetch('age')
+   credentials = params.fetch('credentials')
+   @member = Member.new(name, age, credentials)
+   @member.save()
+   @team = Team.find(params.fetch('team_id').to_i())
+   @team.add_member(@member)
+   erb(:success)
+ end
+end
